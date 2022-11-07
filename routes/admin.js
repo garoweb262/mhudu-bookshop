4286ce86bb06dbef236401f41fffb608a6ac2bba
garoweb262 <muhdgazzali01@gmail.com>
2022-11-07 12:22:06 +0100

    admin logout

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,5 @@ router.post("/login", adminController.login_admin);
 router.get("/dashboard", adminController.get_admin_dashboard);
 
 router.get("/", adminController.get_admin_login);
+router.get("/logout", adminController.get_logout);
 module.exports = router;
